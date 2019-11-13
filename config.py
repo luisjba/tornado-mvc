0486f749f6d5485cf10424eab6014d9fd4d0301a
Jose Luis Bracamonte Amavizca <me@luisjba.com>
2019-11-13 12:27:16 -0700

    Configuration Files

---
+++ config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+app_name = "MVC Tornado App"
+home_controller = "home"
+controllers_path = "controllers"
+views_path = "views"
+assets_path = "assets"
+pid_file = 'mvc_tornado_app.pid'
