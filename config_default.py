fddccc9f084f7427f174b15867859d2a896e2fee
Jose Luis Bracamonte Amavizca <me@luisjba.com>
2019-12-12 15:06:46 -0700

    Adding the configuration for models path

--- config_default.py
+++ config_default.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 app_name = "MVC Tornado App"
 home_controller = "home"
+models_path = "models"
 controllers_path = "controllers"
 views_path = "views"
 assets_path = "assets"
